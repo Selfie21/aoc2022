fe5d4690e3ddccfd45eb3159b6282a1d1508d6bf
Selfie21 <uitxj@student.kit.edu>
2022-11-30 22:16:56 -0300

    created repository for aoc2022

---
+++ 1.py
@@ -0,0 +1,14 @@
+p1 = None
+p2 = None
+
+#file = open("1.in", "r")
+#lines = file.read().splitlines()
+#lines = [int(line) for line in lines]
+
+for line in open('1.in', "r"):
+    line = line.strip()
+    print(line)
+
+
+print(p1)
+print(p2)
